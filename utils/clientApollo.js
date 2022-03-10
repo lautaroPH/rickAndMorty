6de29f1a5bbe1aa91fc4b16144bd47f5905d4ff3
lautaroPH <lautaroph@gmail.com>
2022-03-10 15:16:09 -0300

    api graphql

--- utils/clientApollo.js
+++ utils/clientApollo.js
@@ -3,6 +3,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 export const clientApollo = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://rick-and-morty.app.vercel/api/graphql',
+    uri: 'https://rickandmortyapi.com/graphql',
   }),
 });
