cfcd245f8b1724c98e33ce4960151e49eff9e978
lautaroPH <lautaroph@gmail.com>
2022-03-10 15:51:05 -0300

    fix api

--- pages/index.js
+++ pages/index.js
@@ -7,62 +7,61 @@ import useNearScreen from '../hooks/useNearScreen';
 import { ALL_CHARACTER } from '../schemasGraphql/allCharacter';
 import { clientApollo } from '../utils/clientApollo';
 
-export default function Home() {
-  // const [getCharacter, result] = useLazyQuery(ALL_CHARACTER);
-  // const [characters, setCharacters] = useState([]);
-  // const [hasNextPage, sethasNextPage] = useState('');
-  // const [page, setPage] = useState(2);
+export default function Home({ data }) {
+  const [getCharacter, result] = useLazyQuery(ALL_CHARACTER);
+  const [characters, setCharacters] = useState(data.allCharacter);
+  const [hasNextPage, sethasNextPage] = useState(data.hasNextPage);
+  const [page, setPage] = useState(2);
 
-  // const externalRef = useRef();
+  const externalRef = useRef();
 
-  // const { isNearScreen } = useNearScreen({
-  //   externalRef: externalRef,
-  //   once: false,
-  // });
+  const { isNearScreen } = useNearScreen({
+    externalRef: !data.allCharacter ? null : externalRef,
+    once: false,
+  });
 
-  // useEffect(() => {
-  //   if (isNearScreen) {
-  //     getCharacter({ variables: { page } });
-  //   }
-  // }, [page, getCharacter, isNearScreen]);
+  useEffect(() => {
+    if (isNearScreen) {
+      getCharacter({ variables: { page } });
+    }
+  }, [page, getCharacter, isNearScreen]);
 
-  // useEffect(() => {
-  //   if (
-  //     result?.data?.allCharacter &&
-  //     result?.data?.hasNextPage !== hasNextPage &&
-  //     hasNextPage
-  //   ) {
-  //     setCharacters((character) =>
-  //       [...character, result?.data?.allCharacter].flat(Infinity)
-  //     );
-  //     setPage(page + 1);
-  //     sethasNextPage(result?.data?.hasNextPage);
-  //   }
-  // }, [result, hasNextPage, page]);
+  useEffect(() => {
+    if (
+      result?.data?.allCharacter &&
+      result?.data?.hasNextPage !== hasNextPage &&
+      hasNextPage
+    ) {
+      setCharacters((character) =>
+        [...character, result?.data?.allCharacter].flat(Infinity)
+      );
+      setPage(page + 1);
+      sethasNextPage(result?.data?.hasNextPage);
+    }
+  }, [result, hasNextPage, page]);
 
   return (
-    // <>
-    //   <Header />
-    //   <CharactersList
-    //     characters={characters}
-    //     noMoreCharacters={hasNextPage}
-    //     loading={result?.loading}
-    //   />
-    //   <div id="visor" ref={externalRef}></div>
-    // </>
-    <div>hola puta</div>
+    <>
+      <Header />
+      <CharactersList
+        characters={characters}
+        noMoreCharacters={hasNextPage}
+        loading={result?.loading}
+      />
+      <div id="visor" ref={externalRef}></div>
+    </>
   );
 }
 
-// export async function getStaticProps() {
-//   const { data } = await clientApollo.query({
-//     query: ALL_CHARACTER,
-//     variables: { page: 1 },
-//   });
+export async function getStaticProps() {
+  const { data } = await clientApollo.query({
+    query: ALL_CHARACTER,
+    variables: { page: 1 },
+  });
 
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
+  return {
+    props: {
+      data,
+    },
+  };
+}
--- utils/clientApollo.js
+++ utils/clientApollo.js
@@ -3,6 +3,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 export const clientApollo = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'https://rickandmortyapi.com/graphql',
+    uri: 'https://rick-and-morty-theta-three.vercel.app/api/graphql',
   }),
 });
