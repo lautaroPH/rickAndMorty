acf1587c75d83845b30f2bbe0ea5fbdef30eb7c0
lautaroPH <lautaroph@gmail.com>
2022-03-10 15:21:03 -0300

    api graphql

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ import useNearScreen from '../hooks/useNearScreen';
 import { ALL_CHARACTER } from '../schemasGraphql/allCharacter';
 import { clientApollo } from '../utils/clientApollo';
 
-export default function Home({ data }) {
+export default function Home() {
   const [getCharacter, result] = useLazyQuery(ALL_CHARACTER);
-  const [characters, setCharacters] = useState(data.allCharacter);
-  const [hasNextPage, sethasNextPage] = useState(data.hasNextPage);
+  const [characters, setCharacters] = useState([]);
+  const [hasNextPage, sethasNextPage] = useState('');
   const [page, setPage] = useState(2);
 
   const externalRef = useRef();
 
   const { isNearScreen } = useNearScreen({
-    externalRef: !data.allCharacter ? null : externalRef,
+    externalRef: externalRef,
     once: false,
   });
 
@@ -53,15 +53,15 @@ export default function Home({ data }) {
   );
 }
 
-export async function getStaticProps() {
-  const { data } = await clientApollo.query({
-    query: ALL_CHARACTER,
-    variables: { page: 1 },
-  });
+// export async function getStaticProps() {
+//   const { data } = await clientApollo.query({
+//     query: ALL_CHARACTER,
+//     variables: { page: 1 },
+//   });
 
-  return {
-    props: {
-      data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
